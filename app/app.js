db27cf99ff9afb8a231c8f1edd96a1ea4276dcc1
Susan Rehl <susanrehl@gmail.com>
2017-02-16 09:11:28 -0600

    Test for all users completed.

--- app/app.js
+++ app/app.js
@@ -8,5 +8,5 @@
     $urlRouterProvider.otherwise("/");
   });
 
-  console.log("hello from app.js");
+
 })();
---
+++ app/services/users/users.spec.js
@@ -0,0 +1,76 @@
+describe('Users factory', function() {
+
+//TEST 1
+//  it('has a dummy spec to test 2 + 2', function() {
+     // An intentionally failing test. No code within expect() will never equal 4.
+    // expect().toEqual(4);   toEqual(4) is the chained matcher function, 4 is the actual.This is the expected output
+
+//TEST 2
+     // Adding code to expect(). This test should pass
+    // expect(2 + 2).toEqual(4);
+
+  // });
+
+//TEST 3
+  var Users;
+
+  // The array of users our factory will provide us
+    var userList = [
+      {
+        id: '1',
+        name: 'Jane',
+        role: 'Designer',
+        location: 'New York',
+        twitter: 'gijane'
+      },
+      {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob'
+      },
+      {
+        id: '3',
+        name: 'Jim',
+        role: 'Developer',
+        location: 'Chicago',
+        twitter: 'jimbo'
+      },
+      {
+        id: '4',
+        name: 'Bill',
+        role: 'Designer',
+        location: 'LA',
+        twitter: 'dabill'
+      }
+    ];
+
+
+    // Before each test load our api.users module
+    beforeEach(angular.mock.module('api.users'));   //beforeEach is Jasmine function allowing code to run before test
+
+    // Before each test set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function(_Users_) {
+      Users = _Users_;
+    }));
+
+    // A simple test to verify the Users factory exists
+    it('should exist', function() {
+      expect(Users).toBeDefined();
+    });
+
+//TEST 4
+    // A set of tests for our Users.all() method
+      describe('.all()', function() {
+        // A simple test to verify the method all exists
+        it('should exist', function() {
+          expect(Users.all).toBeDefined();
+        });
+      });
+
+    // A test to verify that calling all() returns the array of users we hard-coded above
+        it('should return a hard-coded list of users', function() {
+          expect(Users.all()).toEqual(userList);
+         });
+        });
